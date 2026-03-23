ace95dcd13b992833e7fb5d22de3d385f8f73374
Muhammad Fajrul Aslim <muhammad.fajrul@wartek.belajar.id>
2026-03-23 17:17:52 +0700

    nested-test-pkg

--- src/plugins/unpkg-path-plugin.tsx
+++ src/plugins/unpkg-path-plugin.tsx
@@ -18,7 +18,7 @@ export const unpkgPathPlugin = () => {
         if(args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           }
         }
         return {
@@ -34,16 +34,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'medium-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
         }
 
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
 
         // if (args.path === 'index.js') {
