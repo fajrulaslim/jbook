c287c9e447781dc64e3139836b9aa004a88a562b
Muhammad Fajrul Aslim <muhammad.fajrul@wartek.belajar.id>
2026-04-04 11:56:02 +0700

    error handling

--- src/bundler/index.tsx
+++ src/bundler/index.tsx
@@ -6,13 +6,14 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 let service: esbuild.Service;
 
 const bundle = async (rawCode: string) => {
-    if (!service) {
-        service = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        })
-    }
+  if (!service) {
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
+    })
+  }
 
+  try {
     const result = await service.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -23,8 +24,19 @@ const bundle = async (rawCode: string) => {
         global: 'window'
       }
     })
+  
+    return {
+      code: result.outputFiles[0].text,
+      err: ''
+    }
 
-    return result.outputFiles[0].text
+  } catch (err: any) {
+    return {
+      code: '',
+      err: err.message
+    }
+  }
+
 }
 
 export default bundle
